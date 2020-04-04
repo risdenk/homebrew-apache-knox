251a89c9fef1a623cd138944352a46e3bf2b6d5d
Kevin Risden <krisden@apache.org>
2020-04-03 21:54:49 -0400

    Try adding head build

--- Formula/apache-knox.rb
+++ Formula/apache-knox.rb
@@ -5,21 +5,33 @@ class ApacheKnox < Formula
   mirror "https://archive.apache.org/dist/knox/1.3.0/knox-1.3.0.tar.gz"
   version "1.3.0"
   sha256 "476e1267c3faee711a6b77b26267d9368e328621ba8ac81869c5e32ec136beb4"
-  head "https://github.com/apache/knox.git"
+
+  head do
+    url "https://github.com/apache/knox.git"
+    depends_on "maven" => :build
+  end
 
   bottle :unneeded
 
   depends_on :java => "1.8"
 
   def install
-    rm_f Dir["bin/*.cmd"]
-    libexec.install Dir["*"]
+    if build.head?
+      system "mvn", "clean", "package", "-pl", "gateway-release", "-am", "-DskipTests", "-Ppackage,release"
+      (buildpath/"target/binaries").mkpath
+      system "tar", "xzf", "target/1.4.0-SNAPSHOT/knox-*-SNAPSHOT.tar.gz", "-C", "target/binaries"
+      rm_f Dir["target/binaries/*/bin/*.cmd"]
+      libexec.install Dir["target/binaries/*/*"]
+    else
+      rm_f Dir["bin/*.cmd"]
+      libexec.install Dir["*"]
 
-    # Some binaries have really generic names (like `test`) and most seem to be
-    # too special-purpose to be permanently available via PATH.
-    #%w[gateway.sh ldap.sh].each do |script|
-    #  (bin/script).write_env_script "#{libexec}/bin/#{script}", Language::Java.java_home_env("1.8")
-    #end
+      # Some binaries have really generic names (like `test`) and most seem to be
+      # too special-purpose to be permanently available via PATH.
+      #%w[gateway.sh ldap.sh].each do |script|
+      #  (bin/script).write_env_script "#{libexec}/bin/#{script}", Language::Java.java_home_env("1.8")
+      #end
+    end
   end
 
   test do
